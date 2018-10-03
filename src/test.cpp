4993fa998e909bfff756f202111a3b9c244441d7
Sam Kagan <sammkagan@gmail.com>
2018-10-02 21:50:01 -0700

    test.cpp now reads in a json file for testing.

--- src/test.cpp
+++ src/test.cpp
@@ -3,21 +3,62 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include <Dense>
 #include "HamHelper.h"
 #include <complex>
 //#include <math.h>
 #include <functional>
 #include <vector>
+#include <string>
+#include "json.hpp"
 
 using namespace std;
 using namespace Eigen;
+using namespace nlohmann;
 
 //MatrixXcd evaluateHammy(vector<function<complex<double>(complex<double>)>> &h, double t);
 
-int main() {
+int main(int argc, char** argv) {
   const float DT = 1/60.0;
   const float SIM_LENGTH = 100;
+
+  //Take in filename from command line
+	//Check for arguments
+	if(argc!=2)
+	{
+		cout<<"Error: Improper number of arguments."<<endl;
+		return -1;
+	}
+	string filename = argv[1];
+
+  //Make sure it's a json file
+  if(!(filename.size() > 5 && filename.substr(filename.size() - 5, 5).compare(".json") == 0)) {
+    cout<<"Error: File is not a JSON file." << endl << endl;
+    return -1;
+  }
+
+	ifstream in(filename);
+
+	//File check
+	if(!(in &&  in.good()))
+	{
+		cout<<"Error: Bad file."<<endl<<endl;
+		return -1;
+	}
+
+  //Read in the JSON file to a JSON object
+  json j;
+  in >> j;
+
+
+  //Making sure the test is of the right type
+  if(!(j["testType"].is_string() && j["testType"].get<string>().compare(string{"HamiltonianSimulation"}) == 0)) {
+    cout << "Error: Test file not of type \"HamiltonianSimulation\"" << endl << endl;
+    return -1;
+  }
+
+  cout << j["testName"] << endl;
 
   //For when the Hamiltonian is of functions of t
   /*
@@ -32,6 +73,14 @@ int main() {
   */
 
 
+  //Read in "hamiltonian" to a MatrixXcd
+  //Read in psi0 to a vector
+  //Read test data into an array of vectors of complex numbers
+  //Read in timeStep and finalTime
+  //Run simulation, checking at each stepwhether test[i] = psi (where i is the index of the loop, and time = i*timeStep,
+    //so loop terminates when i*timStep >= finalTime).
+  //If test[i] != psi, print out the discrepancy with a message and exit.
+
   MatrixXcd hamiltonian = MatrixXcd::Random(3,3) * 10;
 
   MatrixXcd propagator = (-1*DT*hamiltonian).array().exp().matrix();
@@ -41,12 +90,13 @@ int main() {
   cout << "H = " << endl << hamiltonian << endl;
   cout << "U = " << endl << propagator << endl << endl;
 
+  /*
   VectorXcd psi = psi0;
   for (double i = 0; i < SIM_LENGTH * DT; i+=DT) {
     cout << "At time = " << i << ", psi = " << endl << psi << endl;
     psi = propagator * psi;
     cin.get();
-  }
+  }*/
 }
 
 
